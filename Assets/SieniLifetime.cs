9ba919b79adb2762ed9244a528f6fd314dadaa1e
Lari Lunden <lari.lunden@gmail.com>
2019-12-14 21:34:24 +0200

    Prefabs

---
+++ Assets/SieniLifetime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SieniLifetime : MonoBehaviour
+{
+	public float Lifetime = 5.0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Lifetime -= Time.deltaTime;
+		if (Lifetime < 0) {
+			Destroy(this.gameObject);
+		}
+    }
+}
