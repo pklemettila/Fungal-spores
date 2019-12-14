c65ca002a1bc37f65aaffceab366381d5bec4085
Lari Lunden <lari.lunden@gmail.com>
2019-12-14 17:51:34 +0200

    Implemented örmylä spawning

---
+++ Assets/OozingSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OozingSpawn : MonoBehaviour
+{
+	float cloneTimer;
+	float childTimer;
+	
+	public GameObject Clone;
+	public GameObject Child;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloneTimer = Random.Range(15.0f, 20.0f);
+		childTimer = Random.Range(5.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		childTimer -= Time.deltaTime;
+		if (childTimer < 0 && Child != null) {
+			childTimer = Random.Range(5.0f, 10.0f);
+			Instantiate(Child, transform.position, transform.rotation);
+		}
+		
+		cloneTimer -= Time.deltaTime;
+		if (cloneTimer < 0 && Clone != null) {
+			cloneTimer = Random.Range(15.0f, 20.0f);
+			Instantiate(Clone, transform.position, transform.rotation);
+		}     
+    }
+}
